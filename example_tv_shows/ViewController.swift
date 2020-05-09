3b6d9c710daea43393d5b5a09eaa15436b5f4dbd
Alberto Díaz <adiazdi@gmail.com>
2020-05-09 16:53:43 +0200

    Initial Commit

---
+++ example_tv_shows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example_tv_shows
+//
+//  Created by Alberto Díaz Díaz on 09/05/2020.
+//  Copyright © 2020 Alberto Díaz Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ example_tv_showsTests/example_tv_showsTests.swift
@@ -0,0 +1,34 @@
+//
+//  example_tv_showsTests.swift
+//  example_tv_showsTests
+//
+//  Created by Alberto Díaz Díaz on 09/05/2020.
+//  Copyright © 2020 Alberto Díaz Díaz. All rights reserved.
+//
+
+import XCTest
+@testable import example_tv_shows
+
+class example_tv_showsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
